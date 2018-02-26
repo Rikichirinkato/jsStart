cdd556c93ffd57c0f4c06ca133737446d0cf707f
Rom K <roman28krasko@gmail.com>
2018-02-26 19:04:27 +0000

    init

---
+++ js/index.js
@@ -0,0 +1,24 @@
+// let productTitle = 'Mazda';
+// let productPrise = 40000;
+// let productAvaible= true;
+
+// document.write(productTitle);
+// document.write('<br>');
+// document.write(productPrise*0.9);
+// document.write('<br>');
+// document.write(productAvaible);
+// document.write('<br>');
+
+//console.log(1/0)
+
+// const name = prompt('Enter Name');
+// console.log('hello, ' + name)
+// const age = parseInt(prompt());
+
+// age > 17 ? console.log('adult') : console.log('child')
+
+function add(a,b,c){
+    return a+b+c;
+}
+const res = add(11,12,13)
+console.log(res)
